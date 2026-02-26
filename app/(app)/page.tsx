1d795979d8a200dc460fc19de7cd9fda8e631fb0
Kartik Sharma <kk769@gmail.com>
2026-02-26 14:13:25 +0530

    Initial commit

---
+++ app/(app)/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from "@/components/Header";
+import { Divide } from "lucide-react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      {/* Navigation */}
+      <Header />
+    </div>
+  );
+}
